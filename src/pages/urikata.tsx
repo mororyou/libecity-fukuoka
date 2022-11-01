6c29a087d713b07db6e7230ca04a2eca82450c73
tomare-moroizumi <m.ryousuke0401@icloud.com>
2022-11-01 21:13:03 +0900

    売り方研究室追加

---
+++ src/pages/urikata.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+import EventPage from '../components/Event/EventPage'
+
+const Urikata: NextPage = () => (
+  <>
+    <Head>
+      <title>リベシティ　売り方研究室イベント管理</title>
+    </Head>
+    <EventPage prefectures={'51'} prefecturesName={'売り方研究室'} />
+  </>
+)
+
+export default Urikata
