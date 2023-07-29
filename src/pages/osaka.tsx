39498afd953f9c99a760c72fe2f1617ed07f3ce7
tomare-moroizumi <m.ryousuke0401@icloud.com>
2023-07-29 13:48:51 +0900

    updated osaka page

---
+++ src/pages/osaka.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+import EventPage from '../components/Event/EventPage'
+
+const Niigata: NextPage = () => (
+  <>
+    <Head>
+      <title>リベシティ　大阪支部イベント管理</title>
+    </Head>
+    <EventPage prefectures={'27'} prefecturesName={'大阪支部'} />
+  </>
+)
+
+export default Niigata
