fe7e65f57c5ef8bfffea2a252582fec83329d9af
R.Moroizumi <m.ryousuke0401@icloud.com>
2023-11-25 22:30:20 +0900

    Updated FileName remake Nagoya->Tokai

--- src/pages/tokai.tsx
+++ src/pages/tokai.tsx
@@ -5,9 +5,9 @@ import EventPage from '../components/Event/EventPage'
 const Nagoya: NextPage = () => (
   <>
     <Head>
-      <title>リベシティ　名古屋支部イベント管理</title>
+      <title>リベシティ　東海支部イベント管理</title>
     </Head>
-    <EventPage prefectures={'56'} prefecturesName={'名古屋支部'} />
+    <EventPage prefectures={'56'} prefecturesName={'東海支部'} />
   </>
 )
 
